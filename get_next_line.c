74c98868780428eda57e6ca1675cb72581273114
=Facinet Kouyate <=fakouyat@student.42wolfsburg.de>
2022-05-25 16:54:20 +0200

    memory leaks fixing

--- get_next_line.c
+++ get_next_line.c
@@ -12,32 +12,33 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-static void	ft_read_line(int fd, char **str_kept,
-			char **str_read, char **str_line)
+static void	ft_read_line(int fd, char **str_kept, char **str_line)
 {
 	char	*index;
-	int		count_read;
+	int		count;
+	char	*str_read;
 
-	*str_kept = ft_strjoin(*str_kept, *str_read);
-	index = ft_strchr(*str_kept, '\n');
-	if (index)
-	{
-		*str_line = ft_substr(*str_kept, 0, (index - *str_kept + 1));
-		if (!(index + 1))
-			*str_kept = "";
-		else
-			*str_kept = index + 1;
-	}
+	str_read = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+	count = read(fd, str_read, BUFFER_SIZE);
+	if (count < 0)
+		*str_line = 0;
 	else
 	{
-		count_read = read(fd, *str_read, BUFFER_SIZE);
-		if (count_read <= 0)
+		*str_kept = ft_strjoin(*str_kept, str_read);
+		free(str_read);
+		index = ft_strchr(*str_kept, '\n');
+		if (index)
 		{
-			*str_line = *str_kept;
-			*str_kept = "";
+			*str_line = ft_substr(*str_kept, 0, (index - *str_kept) + 1);
+			*str_kept = index + 1;
 		}
 		else
-			ft_read_line(fd, str_kept, str_read, str_line);
+		{
+			if (count != 0)
+				ft_read_line(fd, str_kept, str_line);
+			else
+				*str_line = *str_kept;
+		}
 	}
 }
 
@@ -45,28 +46,28 @@ char	*get_next_line(int fd)
 {
 	static char	*str_stored;
 	char		*line;
-	char		*str_buff;
-	int			count;
 
 	if (!str_stored)
 		str_stored = "";
-	str_buff = (char *)malloc(sizeof(char) * BUFFER_SIZE);
-	if (!str_buff)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	count = read(fd, str_buff, BUFFER_SIZE);
-	if (count <= 0 && str_stored[0] == 0)
-		return (NULL);
-	ft_read_line(fd, &str_stored, &str_buff, &line);
-	
-	if (!(line[0] != 0))
+	ft_read_line(fd, &str_stored, &line);
+	if (line[0] == 0)
 		return (NULL);
 	return (line);
 }
 
-// int main(void)
-// {
-// 	int fd;	
-// 	fd = open("texte.txt", O_RDONLY);
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// }
+int main(void)
+{
+	int fd;	
+	char *line;
+	int i;
+	fd = open("texte.txt", O_RDONLY);
+	i = 0;
+	while (i < 10)
+	{
+		line = get_next_line(fd);
+		printf("%s", line);
+		i++;
+	}
+}
