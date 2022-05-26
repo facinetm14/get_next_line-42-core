49700ab4abd1b8b5ba6a87d87991018b7ce1d637
=Facinet Kouyate <=fakouyat@student.42wolfsburg.de>
2022-05-26 04:08:38 +0200

    some break

--- get_next_line.c
+++ get_next_line.c
@@ -12,36 +12,39 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-static void	ft_read_line(int fd, char **str_kept, char **str_line)
+static void	free_mem(void *pointer)
+{
+	free(pointer);
+	pointer = 0;
+}
+
+static char	*ft_read_line(int fd, char **str_kept)
 {
 	char	*index;
-	int		count;
 	char	*str_read;
+	char	*line;
+	int		count;
 
 	str_read = (char *)malloc(sizeof(char) * BUFFER_SIZE);
 	count = read(fd, str_read, BUFFER_SIZE);
-
 	if (count <= 0)
 	{
-		*str_line = *str_kept;
-		free(str_read);
+		line = *str_kept;
+		*str_kept = NULL;
+		return (line);
+	}
+	*str_kept = ft_strjoin(*str_kept, str_read);
+	free_mem(str_read);
+	index = ft_strchr(*str_kept, '\n');
+	if (index != 0)
+	{
+		line = ft_substr(*str_kept, 0, (index - *str_kept) + 1);
+		free_mem(*str_kept);
+		*str_kept = index + 1;
 	}
 	else
-	{
-		*str_kept = ft_strjoin(*str_kept, str_read);
-		free(str_read);
-		str_read = 0;
-		index = ft_strchr(*str_kept, '\n');
-		if (index != 0)
-		{
-			*str_line = ft_substr(*str_kept, 0, (index - *str_kept) + 1);
-			*str_kept = index + 1;
-		}
-		else
-			return ft_read_line(fd, str_kept, str_line);
-	}
-	if (count == 0 && *str_kept != 0)
-		*str_kept = 0;
+		return (ft_read_line(fd, str_kept));
+	return (line);
 }
 
 char	*get_next_line(int fd)
@@ -53,23 +56,22 @@ char	*get_next_line(int fd)
 		str_stored = "";
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	ft_read_line(fd, &str_stored, &line);
-	if (line[0] == 0)
-		return (NULL);
+	line = ft_read_line(fd, &str_stored);
 	return (line);
 }
 
-// int main(void)
-// {
-// 	int fd;	
-// 	int i;
-// 	fd = open("/Users/fakouyat/Project/get_next_line/gnlTester/files/43_no_nl", O_RDONLY);
-// 	i = 0;
-// 	while (i <=1)
-// 	{
-// 		printf("%s", get_next_line(fd));
-// 		i++;
-// 		printf("%s", get_next_line(fd));
-// 		i++;
-// 	}
-// }
+int main(void)
+{
+	int fd;	
+	int i;
+	fd = open("bible.txt", O_RDONLY);
+	i = 0;
+	char *line;
+	while (i < 2)
+	{
+		line = get_next_line(fd);
+		printf("%s", line);
+		line = 0;
+		i++;
+	}
+}
