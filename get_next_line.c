40befcf41886c21b3b69742367bbae73ac512a5b
=Facinet Kouyate <=fakouyat@student.42wolfsburg.de>
2022-05-27 22:38:55 +0200

    endpoint

--- get_next_line.c
+++ get_next_line.c
@@ -12,66 +12,47 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-static void	free_mem(void *pointer)
-{
-	free(pointer);
-	pointer = 0;
-}
-
-static char	*ft_read_line(int fd, char **str_kept)
+int	ft_check(char **str_kept, char **line)
 {
 	char	*index;
-	char	*str_read;
-	char	*line;
-	int		count;
 
-	str_read = (char *)malloc(sizeof(char) * BUFFER_SIZE);
-	count = read(fd, str_read, BUFFER_SIZE);
-	if (count <= 0)
-	{
-		line = *str_kept;
-		*str_kept = NULL;
-		return (line);
-	}
-	*str_kept = ft_strjoin(*str_kept, str_read);
-	free_mem(str_read);
 	index = ft_strchr(*str_kept, '\n');
-	if (index != 0)
+	if (index)
 	{
-		line = ft_substr(*str_kept, 0, (index - *str_kept) + 1);
-		free_mem(*str_kept);
+		*line = ft_substr(*str_kept, 0, (index - *str_kept) + 1);
 		*str_kept = index + 1;
+		if (*(index + 1) == 0)
+			free(*str_kept);
+		return (1);
 	}
-	else
-		return (ft_read_line(fd, str_kept));
-	return (line);
+	return (0);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*str_stored;
+	static int	count;
+	static char	*str_kept;
+	char		*str_read;
 	char		*line;
 
-	if (!str_stored)
-		str_stored = "";
+	if (!str_kept)
+		str_kept = "";
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	line = ft_read_line(fd, &str_stored);
-	return (line);
-}
-
-int main(void)
-{
-	int fd;	
-	int i;
-	fd = open("bible.txt", O_RDONLY);
-	i = 0;
-	char *line;
-	while (i < 2)
+	if (ft_check(&str_kept, &line) == 1)
+		return (line);
+	str_read = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	count = read(fd, str_read, BUFFER_SIZE);
+	if (count == 0)
 	{
-		line = get_next_line(fd);
-		printf("%s", line);
-		line = 0;
-		i++;
+		line = str_kept;
+		str_kept = NULL;
+		free(str_read);
+		return (line);
 	}
+	str_kept = ft_strjoin(str_kept, ft_substr(str_read, 0, count));
+	free(str_read);
+	if (count < 0)
+		return (NULL);
+	return (get_next_line(fd));
 }
