4a9111c9534eca5ee16d987aed0bf8ace75a7f1c
=Facinet Kouyate <=fakouyat@student.42wolfsburg.de>
2022-05-29 14:55:54 +0200

    done

--- get_next_line.c
+++ get_next_line.c
@@ -10,49 +10,68 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
-#include <stdio.h>
 
-int	ft_check(char **str_kept, char **line)
+int	ft_read_line(int fd, char **str_kept, char **line, int *count)
 {
+	char	str_read[BUFFER_SIZE + 1];
 	char	*index;
+	char	*tmp;
 
 	index = ft_strchr(*str_kept, '\n');
-	if (index)
+	if (index != 0)
 	{
-		*line = ft_substr(*str_kept, 0, (index - *str_kept) + 1);
-		*str_kept = index + 1;
-		if (*(index + 1) == 0)
-			free(*str_kept);
+		tmp = ft_substr(*str_kept, 0, ((index - *str_kept) + 1));
+		*line = tmp;
+		if (*(index + 1) != 0)
+			*str_kept = index + 1;
+		else
+			*str_kept = "";
 		return (1);
 	}
+	*count = read(fd, str_read, BUFFER_SIZE);
+	str_read[*count] = 0;
+	tmp = ft_strjoin(*str_kept, str_read);
+	*str_kept = tmp;
 	return (0);
 }
 
 char	*get_next_line(int fd)
 {
-	static int	count;
 	static char	*str_kept;
-	char		*str_read;
+	static int	count;
 	char		*line;
 
+	count = 0;
 	if (!str_kept)
 		str_kept = "";
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (ft_check(&str_kept, &line) == 1)
+	if (ft_read_line(fd, &str_kept, &line, &count) == 1)
 		return (line);
-	str_read = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-	count = read(fd, str_read, BUFFER_SIZE);
 	if (count == 0)
 	{
 		line = str_kept;
-		str_kept = NULL;
-		free(str_read);
+		str_kept = "";
 		return (line);
 	}
-	str_kept = ft_strjoin(str_kept, ft_substr(str_read, 0, count));
-	free(str_read);
 	if (count < 0)
 		return (NULL);
 	return (get_next_line(fd));
+}
+
+#include <stdio.h>
+int main(void)
+{
+	int fd = open("file.txt", O_RDONLY);
+	char *test;
+	int i;
+	i = 1;
+	while (i <= 39)
+	{
+		test = get_next_line(fd);
+		printf("%s", test);
+		free(test);
+		i++;
+	}
+	return (0);
 }
